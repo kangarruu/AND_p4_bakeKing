b4e596a6526a912ef4c9c79d7d55b50163705e69
Ru Levin <rulevin@gmail.com>
2020-02-05 15:31:22 -0500

    Add verticalStepper button functionality

--- app/src/main/java/com/example/and_p4_bakeking/ui/StepsFragment.java
+++ app/src/main/java/com/example/and_p4_bakeking/ui/StepsFragment.java
@@ -1,6 +1,8 @@
 package com.example.and_p4_bakeking.ui;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +12,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.and_p4_bakeking.R;
 import com.example.and_p4_bakeking.models.Recipe;
@@ -82,8 +87,41 @@ public class StepsFragment extends Fragment implements IStepperAdapter {
     }
 
     @Override
-    public View onCreateCustomView(int position, Context context, VerticalStepperItemView verticalStepperItemView) {
-        return null;
+    public View onCreateCustomView(int position, Context context, VerticalStepperItemView parent) {
+        View viewToInflate = LayoutInflater.from(context).inflate(R.layout.stepper_list_item, parent, false);
+        TextView contentView = viewToInflate.findViewById(R.id.stepper_content_tv);
+        contentView.setText(mStepsList.get(position).getDescription());
+
+        Button nextButton = viewToInflate.findViewById(R.id.stepper_next_btn);
+        Button backButton = viewToInflate.findViewById(R.id.stepper_back_btn);
+
+        //Set the initial button text to "Let's go"
+        //Remove tint during last step
+        if (position == 0){
+            nextButton.setText(getString(R.string.step_list_item_btn_lets_go));
+        }else if(position == mStepsList.size() -1) {
+            nextButton.setBackgroundTintList(getResources().getColorStateList(R.color.material_grey_500));
+            backButton.setBackgroundTintList(getResources().getColorStateList(R.color.material_blue_500));
+        }
+
+        //Set the clickListeners
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStepperView.nextStep();
+                }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStepperView.prevStep();
+            }
+        });
+
+
+
+        return viewToInflate;
     }
 
     @Override
